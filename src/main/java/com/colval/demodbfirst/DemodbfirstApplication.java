f4d6a4084bf67b280a8a2a4d743cc083d062addf
emeri <emeriklatour@gmail.com>
2021-02-15 08:28:37 -0500

    Initial commit

---
+++ src/main/java/com/colval/demodbfirst/DemodbfirstApplication.java
@@ -0,0 +1,13 @@
+package com.colval.demodbfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodbfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodbfirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colval/demodbfirst/DemodbfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colval.demodbfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodbfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
